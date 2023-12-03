0c6d6a1561854e02b55bde2261ffe6231de7808a
Rishav Jaiswal <rishavjaiswal02@gmail.com>
2023-12-03 16:05:18 +0530

    add responsive code for tablets & mobiles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Movie details - https://omdbapi.com/?i=tt4154796&apikey=878d3a94
 
 
-// Variable
+// Variables
 let searchBox = document.querySelector("#movie-search");
 let MovieResults = document.querySelector('.search-results');
 let movieDetail = document.querySelector('.movie-detail-container');
@@ -16,13 +16,14 @@ let factContainer = document.querySelector('.fact');
 let facts = [{fact:"Tom Cruise Supremacy At The Box Office: Mission: Impossible Star Holds The Guinness World Record For Having Not 3, 4 Or 5 But 8 Consecutive $100 Million Movies To His Credit",img:"images/1.jpg"},{fact:"Yash Penned Most Of His Powerful Dialogues By His Own In The Film",img:"images/2.jpg"},{fact:"Shah Rukh Khan Is The Only Indian Actor With 14 Films Earning $10 Million Or More At The Overseas Box Office",img:"images/3.jpg"},{fact:"Zoe Saldana With Avatar 2, Avengers: Endgame & 2 Others Is The Only Actor To Have Four $2 Billion Movies In The Kitty",img:"images/4.jpg"},{fact:"Shah Rukh Khan’s Kabhi Haa Kabhi Naa Took 12 Years To Recover Its Budget",img:"images/5.jpg"},{fact:"The Dark Knight Rises: Anne Hathaway’s Salary Was 3X Of What Tom Hardy Charged As His Fees, & Remember It Was For A Screen Time Of Less Than 20 Minutes!",img:"images/6.jpg"},
 {fact:"Jr NTR Received This Much As Salary For His Debut Film & It’s 112400% Less When Compared With His RRR Fees",img:"images/7.jpg"}, {fact:"KGF Chapter 2 Has Sold The Most Tickets On BookMyShow By Beating Baahubali 2",img:"images/8.jpg"}, {fact:"Inception’s Home Media Revenue Is As High As Its Budget",img:"images/9.jpg"}, {fact:"Before Robert Pattinson, Around 5000 Actors Had Auditioned For Twilight",img:"images/10.jpg"}];
 
-
+// Pick ramdon object from facts array
 function movieFact(){
     let i = Math.floor(Math.random() * 9)
 factHeadline.innerHTML = `${facts[i].fact}`
 factImage.innerHTML = `<img src=${facts[i].img} alt="fact-image">`
 }
 
+// Adding movie in Favourite list using localStorage
 function addToFav(id){
     if(favList == null){
         favList = []
@@ -36,6 +37,7 @@ function addToFav(id){
     alert("Added to your Favourites list")
 }
 
+// Removing movie from Favourite list using localStorage
 function deleteFav(id){
     favList = favList.filter((ele)=>{
         return ele != id
@@ -45,6 +47,8 @@ function deleteFav(id){
     window.location.replace('fav.html');
 }
 
+
+// Fetching data from the API using searchTerm parameter & creating movie cards for each movie
 async function moviesList(searchTerm){
     const url = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=878d3a94`;
     const res = await fetch(url);
@@ -85,6 +89,7 @@ async function moviesList(searchTerm){
 
 }
 
+// calling movieList function on each key pressed & taking input value as a argument
 if(searchBox != null){
     searchBox.addEventListener('input', async()=>{
         const searchTerm = searchBox.value.trim()
@@ -93,7 +98,7 @@ if(searchBox != null){
 }
 
 
-
+// Fetching data from the API using ID & creating movie detail section
 async function movieDetailed(){
     let movieURL = new URLSearchParams(window.location.search);
     let id = movieURL.get('id');
@@ -126,7 +131,7 @@ async function movieDetailed(){
 
 }
 
-
+// Fetching movie details using ID stored in localstorage & creating cards for each movie
 async function favMovies(){
     if(favList == null || favList.length == 0){
         return
